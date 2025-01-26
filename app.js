065e9b5957728d9331025dbcc4b67119be924d7e
SantiagoSauma <lssaumap@gmail.com>
2025-01-26 11:16:14 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let infoButton = document.querySelector(".infoButton")
+let socialButton = document.querySelector(".socialButton")
+let informationInput = document.querySelector(".informationInput")
+let socialInput = document.querySelector(".socialInput")
+
+function hideAll() {
+    informationInput.classList.add("hidden")
+    socialInput.classList.add("hidden")
+}
+
+infoButton.addEventListener('click', () => {
+    hideAll()
+    informationInput.classList.toggle("hidden")
+})
+
+socialButton.addEventListener('click', () => {
+    hideAll()
+    socialInput.classList.toggle("hidden")
+})
